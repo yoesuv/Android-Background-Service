009e96669c42be807570ada746abefea8fcb765a
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-10-31 10:19:53 +0700

    :pencil: apply view binding

--- app/src/main/java/com/yoesuv/androidbackgroundservice/MainActivity.kt
+++ app/src/main/java/com/yoesuv/androidbackgroundservice/MainActivity.kt
@@ -2,10 +2,27 @@ package com.yoesuv.androidbackgroundservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.yoesuv.androidbackgroundservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupButton()
+    }
+
+    private fun setupButton() {
+        binding.btnStart.setOnClickListener {
+            Log.d("result_debug", "MainActivity # START")
+        }
+        binding.btnStop.setOnClickListener {
+            Log.d("result_debug", "MainActivity # STOP")
+        }
     }
 }
