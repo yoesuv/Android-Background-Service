47191ea3bbd3e4e81c389c1bb02948e46a320871
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-01-06 17:06:52 +0700

    :fire: remove data store

--- app/src/main/java/com/yoesuv/androidbackgroundservice/prefs/Store.kt
+++
@@ -1,7 +0,0 @@
-package com.yoesuv.androidbackgroundservice.prefs
-
-import android.content.Context
-import androidx.datastore.preferences.preferencesDataStore
-import com.yoesuv.androidbackgroundservice.data.STORE_NAME
-
-val Context.appStore by preferencesDataStore( name = STORE_NAME)
