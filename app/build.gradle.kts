9534e6c9d88705c059399d10450922c8723f6e97
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-04-14 09:53:53 +0700

    :tractor: migrate kotlin dsl

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    namespace = "com.yoesuv.androidbackgroundservice"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.yoesuv.androidbackgroundservice"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.2.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        setProperty("archivesBaseName", "$applicationId-v$versionCode($versionName)")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.androidx.work)
+    implementation(libs.androidx.preferences)
+}
